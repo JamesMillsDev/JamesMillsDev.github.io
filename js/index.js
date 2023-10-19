da971faada0622eb35399bd17236c4810f349d5f
James Mills <42859892+JamesMillsDev@users.noreply.github.com>
2023-10-20 09:56:10 +1100

    working on new custom site

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
-import '@material/web/all.js'
+const button = document.body.querySelector('#menu-button');
+const menu = document.body.querySelector('#nav-menu');
+
+button.addEventListener('click', () => { menu.open = !menu.open; });
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("header");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+} 
